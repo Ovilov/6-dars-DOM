341036755fabbe1da1f8681d6bab465e2928a04b
Ovilov <ovilovsaidakbar@gmail.com>
2024-03-26 13:38:15 +0500

    add

--- js/app.js
+++ js/app.js
@@ -3,18 +3,31 @@ import data from "./data.js";
 const template = document.querySelector(".job__template");
 const ul = document.querySelector(".job__list");
 
-data.forEach((job) => {
-  const { company, location, position, postedAt, logo, contract } = job;
+data.forEach(() => {
+  const {
+    company,
+    location,
+    position,
+    postedAt,
+    logo,
+    contract,
+    lokation,
+    languages,
+    role,
+    level,
+    tools,
+    featured,
+  } = job;
   const li = template.content.cloneNode(true);
 
   const image = li.querySelector(".job__img");
-  const time = li.querySelector(".job__time");
-  const title = li.querySelector(".job__title");
   const Company = li.querySelector(".job__company");
+  const title = li.querySelector(".job__title");
+  const time = li.querySelector(".job__time");
   const place = li.querySelector(".job__place");
 
   image.src = logo;
-  time.textContent = `${postedAt} | ${contract}`;
+  time.textContent = `${postedAt} | ${contract} | ${lokation}`;
   title.textContent = position;
   Company.textContent = company;
   place.textContent = location;
